120d136c8ee70b1a0125df32c59a5b0aaa3ab790
michellestapp <mstapp@rocketmail.com>
2023-04-12 16:51:29 -0500

    logger file created, lots of bugs -

--- franchise.py
+++ franchise.py
@@ -1,17 +1,22 @@
 from orderfactory import OrderFactory
 from logger import Logger
+from order import Order
 
 
 class Franchise:
     def __init__(self) -> None:
-        self.location_number = input(" Which loaction are you ordering from (1,2,or 3): \n")
+        self.location_number = input(" Which location are you ordering from (1,2,or 3): \n")
+        return self.location_number
         
 
     def place_order(self):
         order = OrderFactory()
-        type = input(" What would you like to order?\n '1' for Pizza\n '2' for Pasta\n '3' for Salad:  ")  
-        meal1 = order.create_order(type)
+        log = Logger()
+
+        self.type = input(" What would you like to order?\n '1' for Pizza\n '2' for Pasta\n '3' for Salad:  ") 
+        meal1 = order.create_order(self.type)
         print(meal1.dish_name, meal1.price)
+        log.log_transaction(self.type,self.location_number)
         
 
 
--- logger.py
+++ logger.py
@@ -5,5 +5,11 @@ class Logger:
         self.transactional_count = 0
         self.daily_sales = 0
 
-    def log_transaction(self):
+    def log_transaction(self, Order, store_number):
         self.order = Order()
+        print(f'You ordered a {self.order.dish_name} which costs {self.order.price} from store location {store_number}')
+        self.transactional_count += 1
+        self.daily_sales += self.order.price
+
+# my_order = Pasta()
+# log_transaction(my_order, 2505)
