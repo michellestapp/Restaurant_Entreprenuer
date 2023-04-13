e43938b06ffe6c16110f3a794ff74ccb9367e599
michellestapp <mstapp@rocketmail.com>
2023-04-13 09:55:08 -0500

    corrected logger call from class to object

--- franchise.py
+++ franchise.py
@@ -1,22 +1,25 @@
 from orderfactory import OrderFactory
 from logger import Logger
-from order import Order
 
 
 class Franchise:
-    def __init__(self) -> None:
+    def __init__(self):
         self.location_number = input(" Which location are you ordering from (1,2,or 3): \n")
-        return self.location_number
+    
         
 
     def place_order(self):
-        order = OrderFactory()
+        orderfactory = OrderFactory()
         log = Logger()
 
+    
+
         self.type = input(" What would you like to order?\n '1' for Pizza\n '2' for Pasta\n '3' for Salad:  ") 
-        meal1 = order.create_order(self.type)
-        print(meal1.dish_name, meal1.price)
-        log.log_transaction(self.type,self.location_number)
+        meal1 = orderfactory.create_order(self.type)
+        print(meal1.dish_name , meal1.price)
+        print(f'You ordered a {meal1.dish_name} which costs ${meal1.price} from store location {self.location_number}')
+        print(meal1)
+        log.log_transaction(meal1,self.location_number)
         
 
 
--- logger.py
+++ logger.py
@@ -1,15 +1,14 @@
-from order import Order
-
 class Logger:
     def __init__(self) -> None:
         self.transactional_count = 0
         self.daily_sales = 0
+       
 
-    def log_transaction(self, Order, store_number):
-        self.order = Order()
-        print(f'You ordered a {self.order.dish_name} which costs {self.order.price} from store location {store_number}')
+    def log_transaction(self, order, store_number):
+
+
         self.transactional_count += 1
-        self.daily_sales += self.order.price
+        print(order)
+        self.daily_sales += order.price
+        print(store_number,order.dish_name, order.price )
 
-# my_order = Pasta()
-# log_transaction(my_order, 2505)
