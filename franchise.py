a03157bd2d0da8063fd66fe3cf8de9ec8ae36f9b
michellestapp <mstapp@rocketmail.com>
2023-04-13 10:51:14 -0500

    defined int per UML

--- franchise.py
+++ franchise.py
@@ -4,7 +4,7 @@ from logger import Logger
 
 class Franchise:
     def __init__(self):
-        self.location_number = input(" Which location are you ordering from (1,2,or 3): \n")
+        self.location_number = int(input(" Which location are you ordering from (1,2,or 3): \n"))
     
         
 
@@ -14,11 +14,11 @@ class Franchise:
 
     
 
-        self.type = input(" What would you like to order?\n '1' for Pizza\n '2' for Pasta\n '3' for Salad:  ") 
+        self.type = int(input(" What would you like to order?\n '1' for Pizza\n '2' for Pasta\n '3' for Salad:  ")) 
         meal1 = orderfactory.create_order(self.type)
         print(meal1.dish_name , meal1.price)
         print(f'You ordered a {meal1.dish_name} which costs ${meal1.price} from store location {self.location_number}')
-        print(meal1)
+        
         log.log_transaction(meal1,self.location_number)
         
 
